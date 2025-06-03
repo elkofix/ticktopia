820871ec81af8eededd79b9e36467eb03ef14a54
elkofix <holasoygermamwow@gmail.com>
2025-06-03 13:11:55 -0500

    chore: changing url to test against deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://localhost:8080',
+  //   url: 'https://www.ticktopia.shop',
   //   reuseExistingServer: !process.env.CI,
   // },
 });
